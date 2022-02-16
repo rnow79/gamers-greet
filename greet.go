0b0a50676133074306b9da0dcd4ee70fb55944d3
Arnau Carrasco <arnau.carrasco@gmail.com>
2022-02-16 18:50:53 +0100

    delete redirection

--- greet.go
+++ greet.go
@@ -11,19 +11,12 @@ import (
 // HTTP server port.
 const port int = 80
 
-// Redirect function.
-func greetRedirection(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/front/index.htm", http.StatusTemporaryRedirect)
-}
-
 // Main function.
 func main() {
 	// Create router.
 	router := mux.NewRouter()
 	// Register static directory.
-	router.PathPrefix("/front").Handler(http.StripPrefix("/front/", http.FileServer(http.Dir("./front/"))))
-	// Register redirection.
-	router.HandleFunc("/", greetRedirection).Methods(http.MethodGet)
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./front/")))
 	// Run web server.
 	server := &http.Server{Handler: router, Addr: ":" + strconv.Itoa(port), WriteTimeout: 10 * time.Second, ReadTimeout: 10 * time.Second}
 	server.ListenAndServe()
